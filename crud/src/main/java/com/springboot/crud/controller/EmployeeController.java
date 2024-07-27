56d0523ccedd03d0f9fda8b1ca2c720bedca7de2
Kalpani Madhushika <kalpaniudawela@gmail.com>
2024-07-27 14:25:13 +0900

    Add CORS resolve angular project connection error

--- crud/src/main/java/com/springboot/crud/controller/EmployeeController.java
+++ crud/src/main/java/com/springboot/crud/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.springboot.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.springboot.crud.entity.Employee;
 import com.springboot.crud.service.EmployeeService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/employee")
 public class EmployeeController {
 	@Autowired
