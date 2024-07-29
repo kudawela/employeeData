6966619156cc8c7724e1fd66878583850b2169a7
Kalpani Madhushika <kalpaniudawela@gmail.com>
2024-07-29 14:27:10 +0900

    Fix PSQL exception of auto increment

--- crud/src/main/java/com/springboot/crud/entity/Employee.java
+++ crud/src/main/java/com/springboot/crud/entity/Employee.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table; //javax
 public class Employee {
 	
 	@Id
-	//@GeneratedValue(strategy = GenerationType.IDENTITY
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String firstName;
 	private String lastName;
